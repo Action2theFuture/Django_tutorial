2a6e36e756c82ecc059fe3e7599918f225ed62fc
Action2theFuture <dnstks0204@google.com>
2021-04-29 16:34:30 +0900

    urls.py/settings.py 충돌 해결

--- wecode_tuto/wecode_tuto/settings.py
+++ wecode_tuto/wecode_tuto/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 	'tuto',
     'movies',
 	'corsheaders'
+    'owners',
 ]
 
 MIDDLEWARE = [
--- wecode_tuto/wecode_tuto/urls.py
+++ wecode_tuto/wecode_tuto/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('movies', include('movies.urls'))
+    path('movies', include('movies.urls')),
+    path('owners', include('owners.urls')),
 ]
